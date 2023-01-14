caaf3704789cb2d713661fb363b312e3ebdb277b
Luismichu <gamba831@gmail.com>
2023-01-14 20:50:39 +0100

    First commit with LibGDX and Idea files

---
+++ desktop/src/com/luismichu/chess/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.luismichu.chess;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.luismichu.chess.MyChess;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Chess");
+		new Lwjgl3Application(new MyChess(), config);
+	}
+}
